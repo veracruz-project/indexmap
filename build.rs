7c819fc20e43da92d201d7badc322ef7f654b844
Shale Xiong <shale.xiong@arm.com>
2020-05-22 17:17:42 +0100

    Switch off mesalock_sgx

--- build.rs
+++ build.rs
@@ -1,8 +1,15 @@
-//extern crate autocfg;
+#[cfg(not(feature = "mesalock_sgx"))]
+extern crate autocfg;
 
+#[cfg(not(feature = "mesalock_sgx"))]
 fn main() {
-    //let ac = autocfg::new();
-    //ac.emit_sysroot_crate("std");
-    //autocfg::rerun_path(file!());
+    let ac = autocfg::new();
+    ac.emit_sysroot_crate("std");
+    autocfg::rerun_path(file!());
+    println!("cargo:rustc-cfg=has_std");
+}
+
+#[cfg(feature = "mesalock_sgx")]
+fn main() {
     println!("cargo:rustc-cfg=has_std");
 }
